05f5f3909c09fc38af4998aca55542c980ca3170
victorduartewz <contato.vhzborges@gmail.com>
2026-04-30 16:19:29 -0300

    feat: add banner img.

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,11 +17,31 @@ const dancingScript = Dancing_Script({
 })
 
 export const metadata: Metadata = {
-  title: "Corretor Vencedor 2026",
+  title: "Gigantes do Mercado Imobiliário - A Evento de Maior Transformação em Vendas do Brasil!",
   description:
     "O evento que entrega a maior inteligência de conversão de vendas do mercado imobiliário",
   icons: {
     icon: "/imersao.favi.png",
+  },
+  openGraph: {
+    title: "Gigantes do Mercado Imobiliário - A Evento de Maior Transformação em Vendas do Brasil!",
+    description:
+      "O evento que entrega a maior inteligência de conversão de vendas do mercado imobiliário",
+    images: [
+      {
+        url: "/banner.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "Gigantes do Mercado Imobiliário 2026",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Gigantes do Mercado Imobiliário - A Evento de Maior Transformação em Vendas do Brasil!",
+    description:
+      "O evento que entrega a maior inteligência de conversão de vendas do mercado imobiliário",
+    images: ["/banner.jpeg"],
   },
 }
 
